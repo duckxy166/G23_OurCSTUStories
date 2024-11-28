923bb83136038f2fd04b6a34c161ac6040437cf9
saharat <saharat.udo@dome.tu.ac.th>
2024-11-28 23:35:46 +0700

    Remove app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let currentSlide = 0;
-const slides = document.querySelector('.slides');
-const totalSlides = document.querySelectorAll('.slide').length;
-
-function changeSlide(direction) {
-    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
-}
